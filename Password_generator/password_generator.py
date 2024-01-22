04d0c64d6f51fc22aa09e2fdaecac95426f719f0
Ielmahi <vodkailias@gmail.com>
2024-01-22 13:15:31 +0100

    👽

--- Password_generator/password_generator.py
+++ Password_generator/password_generator.py
@@ -2,13 +2,13 @@ import random
 import string
 
 print("Welcome to the Password Generator")
-print('_' * 33)
+#print('_' * 33)
 
 num_letters = int(input("How many letters would you like in your password: "))
 num_symbols = int(input("How many symbols would you like: "))
 num_digits = int(input("How many numbers would you like: "))
 
-print('_' * 33)
+#print('_' * 33)
 print("Generating password...")
 
 letters = list(string.ascii_letters)
@@ -23,6 +23,6 @@ password_list = chosen_letters + chosen_symbols + chosen_digits
 random.shuffle(password_list)
 password = ''.join(password_list)
 
-print('_' * 33)
+#print('_' * 33)
 print("Generated Password:")
 print(password)
